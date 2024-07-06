ae556306c7f7333f5f02f84ad5433c8f7c44e0db
kunal-216 <kunaltalreja216@gmail.com>
2024-07-06 16:39:28 +0530

    first commit

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-ordering-backend-eiq2.onrender.com/";
 
   try {
     console.log("Received order request:", req.body);
